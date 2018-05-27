996c852b63bb5b116b7f2e9eff57dd306657c9a9
Edward Mao <ed@edspace.com>
2018-05-27 18:16:23 -0500

    Add energy charge calculations to plan calculator.

--- energycalculator/lib/chargecalc.py
+++ energycalculator/lib/chargecalc.py
@@ -1,3 +1,4 @@
+import logging
 
 class ChargeCalculator(object):
     """Charge calculator
@@ -9,7 +10,27 @@ class ChargeCalculator(object):
         self.plan = plan
 
     def calc(self, kwh):
+        def _filter_schedule(schedule):
+            if(schedule.lower is None):
+                logging.warning("Schedule has a None lower kWh limit")
+                return False
+            if (schedule.upper is None):
+                return schedule.lower <= kwh
+            return schedule.lower <= kwh < schedule.upper
+
+        def _calc_charge(schedule):
+            schedule_charge = 0
+            if(schedule.fixed_charge is not None):
+                schedule_charge += schedule.fixed_charge
+            if(schedule.per_kwh_charge is not None):
+                schedule_charge += schedule.per_kwh_charge * kwh
+            return schedule_charge
+
         total_charge = 0
         energy_charge = self.plan.energy_charges
         total_charge += energy_charge.base
+
+        applicable_schedules = list(filter(_filter_schedule, energy_charge.usage_schedule))
+        fixed_charges = sum(map(_calc_charge, applicable_schedules))
+        total_charge += fixed_charges
         return total_charge
--- tests/energycalculator/lib/test_chargeCalculator.py
+++ tests/energycalculator/lib/test_chargeCalculator.py
@@ -10,15 +10,27 @@ class TestChargeCalculator(TestCase):
             energy_charges=Charges(
                 base=20,
                 usage_schedule=[
-                    UsageSchedule()
+                    UsageSchedule(
+                        lower=0,
+                        upper=100
+                    ),
+                    UsageSchedule(
+                        lower=100,
+                        upper=500,
+                        fixed_charge=28,
+                    )
                 ]
             )
         )
         self.charge_calculator = chargecalc.ChargeCalculator(self.test_plan)
 
-    def test_calc(self):
-        charge = self.charge_calculator.calc(100)
+    def test_calc_base(self):
+        charge = self.charge_calculator.calc(50)
         self.assertEqual(20, charge)
+
+    def test_calc_fixed(self):
+        charge = self.charge_calculator.calc(200)
+        self.assertEqual(48, charge)
 
     def test_create(self):
         self.assertEqual("USD", self.test_plan.currency)
