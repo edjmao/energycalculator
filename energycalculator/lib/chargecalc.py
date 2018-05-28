6b794bc75a011fb79c29239ccb50462e98e75a92
Edward Mao <ed@edspace.com>
2018-05-28 11:22:39 -0500

    Add delivery charge calculation.

--- energycalculator/lib/chargecalc.py
+++ energycalculator/lib/chargecalc.py
@@ -20,17 +20,23 @@ class ChargeCalculator(object):
 
         def _calc_charge(schedule):
             schedule_charge = 0
-            if(schedule.fixed_charge is not None):
+            if schedule.fixed_charge is not None:
                 schedule_charge += schedule.fixed_charge
-            if(schedule.per_kwh_charge is not None):
+            if schedule.per_kwh_charge is not None:
                 schedule_charge += schedule.per_kwh_charge * kwh
             return schedule_charge
 
         total_charge = 0
         energy_charge = self.plan.energy_charges
-        total_charge += energy_charge.base
+        delivery_charge = self.plan.delivery_charges
+        if energy_charge:
+            if energy_charge.base is not None:
+                total_charge += energy_charge.base
+            total_charge += sum(map(_calc_charge, list(filter(_filter_schedule, energy_charge.usage_schedule))))
 
-        applicable_schedules = list(filter(_filter_schedule, energy_charge.usage_schedule))
-        fixed_charges = sum(map(_calc_charge, applicable_schedules))
-        total_charge += fixed_charges
+        if delivery_charge:
+            if delivery_charge.base is not None:
+                total_charge += delivery_charge.base
+            total_charge += sum(map(_calc_charge, list(filter(_filter_schedule, delivery_charge.usage_schedule))))
+
         return total_charge
--- tests/energycalculator/lib/test_chargeCalculator.py
+++ tests/energycalculator/lib/test_chargeCalculator.py
@@ -18,6 +18,21 @@ class TestChargeCalculator(TestCase):
                         lower=100,
                         upper=500,
                         fixed_charge=28,
+                    ),
+                    UsageSchedule(
+                        lower=500,
+                        upper=1000,
+                        per_kwh_charge=.1
+                    ),
+                    UsageSchedule(
+                        lower=1000,
+                        upper=1500,
+                        fixed_charge=10,
+                        per_kwh_charge=.1
+                    ),
+                    UsageSchedule(
+                        lower=2000,
+                        fixed_charge=100
                     )
                 ]
             )
@@ -26,11 +41,43 @@ class TestChargeCalculator(TestCase):
 
     def test_calc_base(self):
         charge = self.charge_calculator.calc(50)
-        self.assertEqual(20, charge)
+        self.assertEqual(20.0, charge)
 
     def test_calc_fixed(self):
         charge = self.charge_calculator.calc(200)
-        self.assertEqual(48, charge)
+        self.assertEqual(48.0, charge)
+
+    def test_calc_per_kwh(self):
+        charge = self.charge_calculator.calc(500)
+        self.assertEqual(70.0, charge)
+
+    def test_calc_and_per_kwh(self):
+        charge = self.charge_calculator.calc(1000)
+        self.assertEqual(130, charge)
+
+    def test_calc_no_base(self):
+        self.test_plan.energy_charges.base=None
+        charge = self.charge_calculator.calc(50)
+        self.assertEqual(0, charge)
+
+    def test_calc_unbounded_upper(self):
+        charge = self.charge_calculator.calc(2500)
+        self.assertEqual(120, charge)
+
+    def test_calc_not_found(self):
+        charge = self.charge_calculator.calc(1750)
+        self.assertEqual(20, charge)
+
+    def test_calc_service_charge(self):
+        self.test_plan.delivery_charges = Charges(
+            base=10,
+            usage_schedule=[UsageSchedule(
+                lower=0,
+                per_kwh_charge=.1
+            )]
+        )
+        charge = self.charge_calculator.calc(100)
+        self.assertEqual(68, charge)
 
     def test_create(self):
         self.assertEqual("USD", self.test_plan.currency)
